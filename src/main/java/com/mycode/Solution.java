a84d1909897ea9d1ae6d3282ed04525122b2f9c7
rnampell <rajashekar.nampelli@gmail.com>
2020-06-14 16:41:33 -0700

    Adding String reverseProgram.

--- src/main/java/com/mycode/Solution.java
+++ src/main/java/com/mycode/Solution.java
@@ -1,7 +1,10 @@
+package com.mycode;
+
 public class Solution {
 
   public static void main(String[] args) {
     //
 
   }
+
 }
---
+++ src/main/java/com/mycode/StringReverse.java
@@ -0,0 +1,31 @@
+package com.mycode;
+
+public class StringReverse {
+
+  public static void main(String[] args) {
+    //
+    String raj = stringReverse("Raj");
+    System.out.println(raj);
+
+    String raj1 = usingStringBuilder("Raj");
+    System.out.println(raj1);
+  }
+
+  public static String stringReverse(String input) {
+    if (input == null) {
+      return null;
+    }
+    String finalString = "";
+    for (int i = input.length() - 1; i >= 0; i--) {
+      finalString = finalString + input.charAt(i);
+    }
+    return finalString;
+  }
+
+  public static String usingStringBuilder(String input) {
+    if (input == null) {
+      return null;
+    }
+    return new StringBuilder(input).reverse().toString();
+  }
+}
